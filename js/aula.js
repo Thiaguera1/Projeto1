e3771c4f21f670a969b9f645be36e8e9fbca1a26
Thiago Camargo <thiagocss13@gmail.com>
2022-06-20 22:37:11 -0300

    commit inicial

---
+++ js/aula.js
@@ -0,0 +1,58 @@
+/*
+let idadeThiago = '20 anos'; string literal
+let alturaThiago = '1,75 metros'; number literal
+let pesoThiago = 70;
+let estaAprovado = true; //boolean
+let sobrenome = undefined; undefined
+let corSelecionado = null // Redefinir um valor
+
+console.log(idadeThiago);
+console.log(alturaThiago);
+console.log(pesoThiago);
+
+const=Fixo    let=Alteravel
+------------------------------------------------------------------
+let nome = 'Thiago';
+let idade = '20';
+let estaAprovado = true; 
+let sobrenome = undefined;
+let altura = '175cm'
+let peso = '70kg'
+let elevesgo = false
+
+let pessoa = {
+    nome: 'Thiago',
+    idade: 20,
+    estaAprovado: true,
+    sobrenome : 'Camargo', 
+    altura,
+    peso,
+    elevesgo
+};
+
+console.log(pessoa);
+*/
+//---------------------------------------------------------------------------
+
+/*arrays 
+let familia = ['false',45,'xagas',17]; //indice do 0 ao nInfinito
+
+console.log(familia[3]);
+
+let nomeAleatoriopraArray = ['Matheus',20 +' e alguma coisa', 'Curitibaloco'];
+
+console.log(nomeAleatoriopraArray);
+
+------------------------------------------------------------------------------------- */
+
+         /*Funções 39:10
+let tipoBlusa = "azul";
+function trocaBlusa(estilo , coloração){
+    tipoBlusa = estilo + ' ' + coloração;
+};
+
+console.log(tipoBlusa);
+trocaBlusa("Maluquinho" , "Rosada");
+console.log(tipoBlusa);
+
+-----------------------------------------------------------------------------------------------*/
